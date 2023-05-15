0a47d0fe44ddb2b2914ed7efd71f79f4e48da01d
Test <irynamyza@gmail.com>
2023-05-15 17:09:42 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,16 +1,19 @@
-class Pizza:
+class Food:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Pizza(Food):
     def __init__(self, name, ingredients, price):
-        self.name = name
+        super().__init__(name, price)
         self.ingredients = ingredients
-        self.price = price
 
-class Drink:
+class Drink(Food):
     def __init__(self, name, description, price):
-        self.name = name
+        super().__init__(name, price)
         self.description = description
-        self.price = price
 
-def place_order(pizzas, drinks):
+def print_menu(pizzas, drinks):
     print("Menu:")
     print("Pizzas:")
     for i, pizza in enumerate(pizzas):
@@ -19,19 +22,22 @@ def place_order(pizzas, drinks):
     for i, drink in enumerate(drinks):
         print(f"{i+1}. {drink.name} - {drink.price} грн")
 
-    order_total = 0
+def order_pizza(pizzas):
     pizza_choice = input("Введіть номер піци: ")
     if pizza_choice.isdigit():
         pizza_index = int(pizza_choice) - 1
         if 0 <= pizza_index < len(pizzas):
             selected_pizza = pizzas[pizza_index]
-            order_total += selected_pizza.price
             print(f"Ви обрали піцу: {selected_pizza.name}")
+            return selected_pizza
         else:
             print(f"Недійсний вибір піци: {pizza_choice}")
     else:
         print(f"Недійсний вибір піци: {pizza_choice}")
+    return None
 
+def order_drink(drinks):
+    order_total = 0
     drink_choice = input("Введіть номер напою: ")
     if drink_choice.isdigit():
         drink_index = int(drink_choice) - 1
@@ -39,12 +45,25 @@ def place_order(pizzas, drinks):
             selected_drink = drinks[drink_index]
             order_total += selected_drink.price
             print(f"Ви обрали напій: {selected_drink.name}")
+            return selected_drink
         else:
             print(f"Недійсний вибір напою: {drink_choice}")
     else:
         print(f"Недійсний вибір напою: {drink_choice}")
+    return None
 
-    print(f"Загальна вартість замовлення: {selected_pizza.name} {selected_drink.name} {order_total} грн")
+def print_order(pizza, drink):
+    if pizza and drink:
+        order_total = pizza.price + drink.price
+        print(f"Загальна вартість замовлення: {order_total} грн")
+        print(f"Піца: {pizza.name}")
+        print(f"Напій: {drink.name}")
+
+def place_order(pizzas, drinks):
+    print_menu(pizzas, drinks)
+    selected_pizza = order_pizza(pizzas)
+    selected_drink = order_drink(drinks)
+    print_order(selected_pizza, selected_drink)
 
 if __name__ == "__main__":
     pizzas = [
